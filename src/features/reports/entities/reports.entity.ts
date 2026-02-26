98f9a7add98851626c3fa893366bd26caa90013c
DiyorbekTurgunov <diyorbekturgunov7803@gmail.com>
2026-02-26 11:34:35 +0500

    update

--- src/features/reports/entities/reports.entity.ts
+++ src/features/reports/entities/reports.entity.ts
@@ -2,13 +2,8 @@ import {Column, Entity, ManyToOne, Timestamp} from "typeorm";
 import {BaseEntity} from "../../../core/Base-module";
 import {ReportCategoriesEntity} from "../../reportCategories/entities/report-Categories.entity";
 import {ReportType} from "../../../core/enums/report-Type.enum";
-import {CoursesEntity} from "../../courses/entities/courses.entity";
 import {CourseReviewsEntity} from "../../courseReviews/entities/course-Reviews.entity";
 import {BookReviewsEntity} from "../../bookReviews/entities/book-Reviews.entity";
-
-function CourseReviewsEntityEntity() {
-
-}
 
 @Entity('reports')
 export class ReportsEntity extends BaseEntity {
@@ -36,6 +31,5 @@ export class ReportsEntity extends BaseEntity {
 
     @Column({ type: 'timestamp'})
     date: Timestamp;
-
 
 }
